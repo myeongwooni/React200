aa29839814d0ed746f8cc3fe2c924268dde5e88c
myeongwooni <uriuni17@naver.com>
2021-03-13 01:25:29 +0900

    210312

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import react from 'react';
 // import './App.css';
-import Sweetalert2Position  from './R057_Sweetalert2Position.js';
+import Promise  from './R065_Promise.js';
 
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <Sweetalert2Position />
+      <Promise />
     </div>
   );
 }
